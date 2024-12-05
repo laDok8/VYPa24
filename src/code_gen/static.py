6233b57515c3ffb96b97d8ae9d2ed2ae8a17ba39
Ladislav Dokoupil <ldokoupi@redhat.com>
2024-12-05 20:21:04 +0100

    substr fix 89/16/105

--- src/code_gen/static.py
+++ src/code_gen/static.py
@@ -41,63 +41,53 @@ GETSIZE {Register.AX} [{Register.SP}]
 SET [{Register.SP}] {Register.AX}
 RETURN [{Register.SP}+1]\n\n'''
 
+
+
     @staticmethod
     def subStr():
         return f'''LABEL subStr
-ADDI {Register.SP} {Register.SP} 2
-SET [{Register.SP}] {Register.BP}
-SET {Register.BP} {Register.SP}
-#[BP-2] = n, [BP-3] = i, [BP-4] = str
-SET $BX [{Register.BP}-3] # i
-
-GETSIZE {Register.AX} [{Register.BP}-4]
-LTI {Register.EX} {Register.BX} 0
-GTI {Register.OBJ} {Register.BX} {Register.AX}
-OR {Register.EX} {Register.EX} {Register.OBJ} # i < 0 || i > size
-
+#[{Register.SP}] = n, [{Register.SP}-1] = i, [{Register.SP}-2] = str
 CREATE {Register.DI} 1
 SETWORD {Register.DI} 0 ""
+GETWORD {Register.DI} {Register.DI} 0
+
+LTI {Register.AX} [{Register.SP}-1] 0
+JUMPNZ subStr_end {Register.AX}
+LTI {Register.AX} [{Register.SP}] 0
+JUMPNZ subStr_end {Register.AX}
+GETSIZE {Register.AX} [{Register.SP}-2]
+LTI {Register.EX} [{Register.SP}-1] {Register.AX}
+NOT {Register.EX} {Register.EX}
 JUMPNZ subStr_end {Register.EX}
 
-RESIZE {Register.DI} [{Register.BP}-2]
-
-SET {Register.EX} [{Register.BP}-2]
-ADDI {Register.EX} {Register.EX} {Register.BX} # n + i
-
-GTI {Register.CX} {Register.EX} {Register.AX} # n + i > size
-JUMPNZ subStr_min_else {Register.CX}
-JUMP subStr_min_end # n + 1 <= size
-LABEL subStr_min_else # n + i > size
-SET {Register.EX} [{Register.BP}-4]
+ADDI {Register.EX} [{Register.SP}-1] [{Register.SP}] # n + i
+GETSIZE {Register.AX} [{Register.SP}-2]
+GTI {Register.CX} {Register.EX} {Register.AX} # n + i > SI.length()
+JUMPZ subStr_min_end {Register.CX}
+SUBI {Register.EX} {Register.AX} [{Register.SP}-1] # n = SI.length() - i
+SET [{Register.SP}] {Register.EX}
 LABEL subStr_min_end
-#SUBI {Register.EX} {Register.EX} 1
-ADDI {Register.SP} {Register.SP} 1
-SET [{Register.SP}] {Register.EX} # [{Register.SP}]=min(n+i,len(str))- 1
 
+CREATE {Register.DI} [{Register.SP}]
 
-SET {Register.AX} 0 #index new
-#SET {Register.BX} {Register.BX} #index old
+SET {Register.CX} 0
+SET {Register.BX} [{Register.SP}-1]
 LABEL subStr_start_while
-LTI {Register.EX} {Register.BX} [{Register.SP}] # i < n+i-1
+LTI {Register.EX} {Register.CX} [{Register.SP}] # i < n
 JUMPZ subStr_end {Register.EX}
 
+GETWORD {Register.AX} [{Register.SP}-2] {Register.BX} # char from SI
+SETWORD {Register.DI} {Register.CX} {Register.AX} # rewrite char
 
-GETWORD {Register.EX} [{Register.BP}-4] {Register.BX} # EX=[{Register.BP}-4][{Register.BX}] - char from SI
-
-SETWORD {Register.DI} {Register.AX} {Register.EX} # rewrite char
-
-ADDI {Register.AX} {Register.AX} 1
+ADDI {Register.CX} {Register.CX} 1
 ADDI {Register.BX} {Register.BX} 1
 JUMP subStr_start_while
 
-
 LABEL subStr_end
-# exit function
-SET {Register.SP} {Register.BP}
-SET {Register.BP} [{Register.SP}]
-SUBI {Register.SP} {Register.SP} 4
+SUBI {Register.SP} {Register.SP} 2
 SET [{Register.SP}] {Register.DI}
-RETURN [{Register.SP}+3]\n\n'''
+RETURN [{Register.SP}+3]
+'''
 
     @staticmethod
     def toString():
